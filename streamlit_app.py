d0fa4bd4374ace4931de1e1439320bfd0998edeb
AlexandraSedlovskaya <55624547+AlexandraSedlovskaya@users.noreply.github.com>
2022-10-26 14:58:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,41 @@
 import streamlit as st
+import tfidf
+import bert
 
-# Store the initial value of widgets in session state
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
+st.title('Поисковик по ответам mail.ru')
 
-col1, col2 = st.columns(2)
+if 'is_expanded' not in st.session_state:
+    st.session_state['is_expanded'] = True
+container = st.expander('Поиск', expanded=st.session_state['is_expanded'])
 
-with col1:
-    st.checkbox("Disable text input widget", key="disabled")
-    st.radio(
-        "Set text input label visibility 👉",
-        key="visibility",
-        options=["visible", "hidden", "collapsed"],
-    )
-    st.text_input(
-        "Placeholder for the other text input widget",
-        "This is a placeholder",
-        key="placeholder",
-    )
+with container:
+    with st.form(key='myform', clear_on_submit=True):
+        query = st.text_input('Запрос', placeholder='Поиск')
+        search_type = st.selectbox('Тип поиска:',  ('TfIdfVectorizer', 'BERT'))
+        submit_button = st.form_submit_button('Искать')
 
-with col2:
-    text_input = st.text_input(
-        "Enter some text 👇",
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-        placeholder=st.session_state.placeholder,
-    )
+        st.session_state['is_expanded'] = False
+if submit_button:
+    if search_type == 'TfIdfVectorizer':
+        st.header('Результаты поиска')
+        results, time = tfidf.main(query)
+        st.write(str(time))
+        for i in range(len(results)-1):
+            print(results[i])
+            st.write(str(i+1), '. Вопрос: ', results[i]['question'])
+            if type(results[i]['comment']) == str:
+                st.write('Комментарий: ' + results[i]['comment'])
+            if results[i]['answer']:
+                st.write('Ответ: ' + results[i]['answer'])
+    else:
+        st.header('Результаты поиска')
+        results, time = bert.main(query)
+        st.write(str(time))
+        for i in range(len(results)-1):
+            print(results[i])
+            st.write(str(i+1), '. Вопрос: ', results[i]['question'])
+            if type(results[i]['comment']) == str:
+                st.write('Комментарий: ' + results[i]['comment'])
+            if results[i]['answer']:
+                st.write('Ответ: ' + results[i]['answer'])
 
-    if text_input:
-        st.write("You entered: ", text_input)
